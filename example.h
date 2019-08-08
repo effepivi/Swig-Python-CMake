9f1da70f3fe87a28ee611d93a911b92ba2f25110
Your Name <you@example.com>
2019-08-08 10:26:38 +0100

    Ad the files.

---
+++ example.h
@@ -0,0 +1,10 @@
+#ifndef example_h
+#define example_h
+
+extern double My_variable;
+
+int fact(int n);
+int my_mod(int x, int y);
+char *get_time();
+
+#endif
---
+++ test_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from example import *
+
+print(get_time())
+print(fact(0))
+print(fact(3))
+print(my_mod(3, 1))
+
